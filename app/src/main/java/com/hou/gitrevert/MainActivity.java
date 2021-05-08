94ea783d29cdb1bfce358c144c3b09be44e27a04
555039392 <houwenhao@sdc.icbc.com.cn>
2021-05-08 09:34:09 +0800

    dev1

--- app/src/main/java/com/hou/gitrevert/MainActivity.java
+++ app/src/main/java/com/hou/gitrevert/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e(TAG,"git revert");
+        Log.e(TAG,"git dev1");
+
     }
 }
