becef6e6c9648b911311ee1d6393d5e3f4674ed1
houwenhao4147 <houwenhao4147@163.com>
2021-05-08 23:21:06 +0800

    增加secondactivity

---
+++ app/src/main/java/com/hou/gitrevert/SecondActivity.java
@@ -0,0 +1,23 @@
+package com.hou.gitrevert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e(TAG,"git revert");
+        Log.e(TAG, "git dev1");
+        Log.e(TAG, "git dev123");
+        Log.e(TAG, "git dev234");
+        Log.e(TAG, "git dev234 bug");
+
+    }
+}
