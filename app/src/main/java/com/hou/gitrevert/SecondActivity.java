11e1a5606b716b545f26bb2777d136392aa303ae
houwenhao4147 <houwenhao4147@163.com>
2021-05-08 23:48:13 +0800

    回退

--- app/src/main/java/com/hou/gitrevert/SecondActivity.java
+++ app/src/main/java/com/hou/gitrevert/SecondActivity.java
@@ -18,7 +18,6 @@ public class SecondActivity extends AppCompatActivity {
         Log.e(TAG, "git dev123");
         Log.e(TAG, "git dev234");
         Log.e(TAG, "git dev234 bug");
-        Log.e(TAG, "git dev345");
 
     }
 }
